5316ad02481536ce3a11bf48bd0d3cd0ba93800d
Victor-louis Castera <vicaster@z2r10p8.42lyon.fr>
2021-04-16 18:23:38 +0200

    Test du resolve TextInput crash

--- front/App.js
+++ front/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import 'react-native-gesture-handler';
 
 import Home from './src/Home.js';
 
 const routes = createStackNavigator(
-	{
-		Home: {
-			screen: Home,
-			navigationOptions: {
-				headerShown: true,
-			}
+{
+	Home: {
+		screen: Home,
+		navigationOptions: {
+			headerShown: false,
 		}
-	},
-	{
-		initialRouteName: 'Home'
-	});
-
+	}
+},
+{
+	initialRouteName: 'Home'
+});
 
 const AppContainer = createAppContainer(routes);
 
