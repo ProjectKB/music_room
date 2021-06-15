2ab50f941296ae5d062b726e2b8e251d4bbb3e16
Loïc Bertin <bertin.loic01@gmail.com>
2021-06-15 18:41:40 +0200

    fix song search pb (callback are the best)

--- client/components/Search/SearchElementList.js
+++ client/components/Search/SearchElementList.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useCallback} from 'react';
 import SearchSong from './SearchSong';
 import PlaylistContent from '../Playlist/PlaylistContent';
 
 const SearchElementList = props => {
-  if (props.chipSelected === 'Song') {
-    return (
-      <SearchSong
-        collection={props.collection}
-        setCollection={props.setCollection}
-        setMaxResults={props.setMaxResults}
-      />
-    );
-  } else if (props.chipSelected === 'Playlist') {
-    return (
-      <PlaylistContent
-        navigation={props.navigation}
-        playlistCollection={props.collection}
-        setPlaylistCollection={props.setCollection}
-        deletionPlaylistModal={props.deletionPlaylistModal}
-        setDeletionPlaylistModal={props.setDeletionPlaylistModal}
-      />
-    );
-  } else {
-    return null;
-  }
+  const SearchElementListCallback = useCallback(() => {
+    if (props.chipSelected === 'Song') {
+      return (
+        <SearchSong
+          collection={props.collection}
+          setCollection={props.setCollection}
+          setMaxResults={props.setMaxResults}
+        />
+      );
+    } else if (props.chipSelected === 'Playlist') {
+      return (
+        <PlaylistContent
+          navigation={props.navigation}
+          playlistCollection={props.collection}
+          setPlaylistCollection={props.setCollection}
+          deletionPlaylistModal={props.deletionPlaylistModal}
+          setDeletionPlaylistModal={props.setDeletionPlaylistModal}
+        />
+      );
+    } else {
+      return null;
+    }
+  }, [props.collection]);
+
+  return <SearchElementListCallback />;
 };
 
 export default SearchElementList;
-
-const styles = StyleSheet.create({});
