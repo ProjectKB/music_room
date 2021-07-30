d13c07f987b14b6f62a3dad2b6d23a1ac15b939b
Loïc Bertin <bertin.loic01@gmail.com>
2021-07-30 21:01:21 +0200

    rework creation modal (change endpoint + add visibility picker)

--- client/api/PlaylistEndpoint.js
+++ client/api/PlaylistEndpoint.js
@@ -6,63 +6,79 @@ export const FetchPlaylistList = async (setter, query) => {
       global.URL + '/playlists/searchPlaylist',
       JSON.stringify(query),
     );
+
     response.data != null ? setter(response.data) : setter([]);
+
     return true;
   } catch (error) {
+    console.log(error.response);
     return false;
   }
 };
+
 export const FetchPlaylistSong = async (setter, query, playlistId) => {
   try {
     const response = await axios.post(
       global.URL + '/playlists/searchSong/' + playlistId,
       JSON.stringify(query),
     );
+
     response.data != null ? setter(response.data) : setter([]);
   } catch (error) {
     console.log(error);
   }
 };
-export const CreatePlaylist = async (setter, playlistName) => {
+
+export const CreatePlaylist = async (
+  setter,
+  playlistName,
+  status,
+  owner_id,
+) => {
   try {
     await axios.post(
       global.URL + '/playlists',
-      JSON.stringify({name: playlistName}),
+      JSON.stringify({name: playlistName, status: status, owner_id: owner_id}),
     );
+
     return FetchPlaylistList(setter, '');
   } catch (error) {
     return false;
   }
 };
+
 export const DeletePlaylist = async playlistId => {
   try {
     await axios.delete(global.URL + '/playlists/' + playlistId);
+
     return true;
   } catch (error) {
     return false;
   }
 };
+
 export const DeleteSong = async (playlistId, songId) => {
   try {
     await axios.put(
       global.URL + '/playlists/removeSong/' + playlistId,
       JSON.stringify({id: songId}),
     );
+
     return true;
   } catch (error) {
-    console.log(error);
     return false;
   }
 };
+
 export const AddSong = async (playlistId, song) => {
   try {
     const response = await axios.put(
       global.URL + '/playlists/addSong/' + playlistId,
       JSON.stringify(song),
     );
+
     return response.data;
   } catch (error) {
-    console.log(error);
     return false;
   }
 };
--- client/components/Playlist/PlaylistCreationModal.js
+++ client/components/Playlist/PlaylistCreationModal.js
@@ -1,13 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {Button} from 'react-native-paper';
 import {CreatePlaylist} from '../../api/PlaylistEndpoint';
 import {FlashMessage} from '../FlashMessage';
+import UserContext from '../../contexts/UserContext';
+import {RadioButton} from 'react-native-paper';
 
 const PlaylistCreationModal = props => {
   const [playlistName, setPlaylistName] = useState('');
+  const [playlistStatus, setPlaylistStatus] = useState('public');
   const [inputIsEmpty, setInputIsEmpty] = useState(false);
+
+  const {user} = useContext(UserContext);
 
   const inputErrorText = inputIsEmpty
     ? '* you have to choose a name for your playlist'
@@ -35,6 +40,32 @@ const PlaylistCreationModal = props => {
         />
         <Text style={styles.textError}>{inputErrorText}</Text>
       </View>
+      <View style={styles.visibilityContainer}>
+        <RadioButton.Group
+          onValueChange={value => {
+            setPlaylistStatus(value);
+          }}
+          value={playlistStatus}>
+          <View style={styles.radioButtonGroup}>
+            <View style={styles.radioButton}>
+              <RadioButton
+                color="#899ed6"
+                value="public"
+                status={playlistStatus === 'public' ? 'checked' : 'unchecked'}
+              />
+              <Text>Public</Text>
+            </View>
+            <View style={styles.radioButton}>
+              <RadioButton
+                color="#899ed6"
+                value="private"
+                status={playlistStatus === 'private' ? 'checked' : 'unchecked'}
+              />
+              <Text>Private</Text>
+            </View>
+          </View>
+        </RadioButton.Group>
+      </View>
       <View style={styles.dialogActionContainer}>
         <Button
           color="#899ed6"
@@ -57,6 +88,8 @@ const PlaylistCreationModal = props => {
               const responseStatus = CreatePlaylist(
                 props.setPlaylistCollection,
                 playlistName,
+                playlistStatus,
+                user.id,
               );
 
               responseStatus.then(status =>
@@ -84,18 +117,24 @@ const styles = StyleSheet.create({
   },
   textInputContainer: {
     backgroundColor: '#f3f1f4',
-    padding: 10,
-    paddingBottom: 5,
+    paddingHorizontal: 10,
+    paddingTop: 10,
     borderTopLeftRadius: 5,
     borderTopRightRadius: 5,
     width: '100%',
+  },
+  visibilityContainer: {
+    backgroundColor: '#f3f1f4',
+    width: '100%',
+    paddingRight: 20,
+    paddingLeft: 10,
+    paddingBottom: 5,
   },
   textError: {
     color: '#b00012',
     fontSize: 12,
     paddingLeft: 5,
-    marginTop: -5,
-    marginBottom: 15,
+    marginTop: -10,
   },
   dialogActionContainer: {
     flexDirection: 'row',
@@ -105,5 +144,15 @@ const styles = StyleSheet.create({
     width: '100%',
     borderBottomEndRadius: 5,
     borderBottomStartRadius: 5,
+  },
+  radioButton: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  radioButtonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
