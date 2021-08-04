e6f833feeaae2bec5e95ad1396815ad2c171fb26
Loïc Bertin <bertin.loic01@gmail.com>
2021-08-04 21:16:41 +0200

    don't show Playlist modal when Search

--- client/components/Playlist/PlaylistContent.js
+++ client/components/Playlist/PlaylistContent.js
@@ -7,54 +7,68 @@ import PlaylistMultiModal from './PlaylistMultiModal';
 import CustomModal from '../CustomModal';
 import PlaylistGuestPickerModal from './PlaylistGuestPickerModal';
 
-const PlaylistContent = props => (
-  <>
-    <ScrollView style={{margin: 10}}>
-      <PlaylistList
-        playlistCollection={props.playlistCollection}
-        navigation={props.navigation}
-        setMultiPlaylistModal={props.setMultiPlaylistModal}
-        setGuestPickerModal={props.setGuestPickerModal}
-        setPlaylistIndex={props.setPlaylistIndex}
-        screen={props.screen}
-        searchQuery={props.searchQuery}
-      />
-    </ScrollView>
-    <CustomModal
-      modalVisibility={props.creationPlaylistModal}
-      setModalVisibility={props.setCreationPlaylistModal}
-      secu={props.creationPlaylistModal}
-      Component={() => (
-        <PlaylistCreationModal
-          setPlaylistCollection={props.setPlaylistCollection}
-          setModalVisibility={props.setCreationPlaylistModal}
-        />
-      )}
-    />
-    <CustomModal
-      modalVisibility={props.multiPlaylistModal}
-      setModalVisibility={props.setMultiPlaylistModal}
-      secu={props.playlistCollection[props.playlistIndex]}
-      Component={() => (
-        <PlaylistMultiModal
+const PlaylistContent = props => {
+  const PlaylistScreenModals = () => {
+    if (props.screen !== 'Search') {
+      return (
+        <>
+          <CustomModal
+            modalVisibility={props.creationPlaylistModal}
+            setModalVisibility={props.setCreationPlaylistModal}
+            secu={props.creationPlaylistModal}
+            Component={() => (
+              <PlaylistCreationModal
+                setPlaylistCollection={props.setPlaylistCollection}
+                setModalVisibility={props.setCreationPlaylistModal}
+              />
+            )}
+          />
+          <CustomModal
+            modalVisibility={props.guestPickerModal}
+            setModalVisibility={props.setGuestPickerModal}
+            secu={props.playlistCollection[props.playlistIndex]}
+            Component={() => (
+              <PlaylistGuestPickerModal
+                guestCollection={props.guestCollection}
+                setModalVisibility={props.setGuestPickerModal}
+                playlist={props.playlistCollection[props.playlistIndex]}
+              />
+            )}
+          />
+        </>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <>
+      <ScrollView style={{margin: 10}}>
+        <PlaylistList
+          playlistCollection={props.playlistCollection}
+          navigation={props.navigation}
           setMultiPlaylistModal={props.setMultiPlaylistModal}
-          playlist={props.playlistCollection[props.playlistIndex]}
+          setGuestPickerModal={props.setGuestPickerModal}
+          setPlaylistIndex={props.setPlaylistIndex}
+          screen={props.screen}
+          searchQuery={props.searchQuery}
         />
-      )}
-    />
-    <CustomModal
-      modalVisibility={props.guestPickerModal}
-      setModalVisibility={props.setGuestPickerModal}
-      secu={props.playlistCollection[props.playlistIndex]}
-      Component={() => (
-        <PlaylistGuestPickerModal
-          guestCollection={props.guestCollection}
-          setModalVisibility={props.setGuestPickerModal}
-          playlist={props.playlistCollection[props.playlistIndex]}
-        />
-      )}
-    />
-  </>
-);
+      </ScrollView>
+      <CustomModal
+        modalVisibility={props.multiPlaylistModal}
+        setModalVisibility={props.setMultiPlaylistModal}
+        secu={props.playlistCollection[props.playlistIndex]}
+        Component={() => (
+          <PlaylistMultiModal
+            setMultiPlaylistModal={props.setMultiPlaylistModal}
+            playlist={props.playlistCollection[props.playlistIndex]}
+          />
+        )}
+      />
+      <PlaylistScreenModals />
+    </>
+  );
+};
 
 export default PlaylistContent;
