f5df7e81acc5e21d3a3cc28d5a65df7ec1246f9a
Loïc Bertin <bertin.loic01@gmail.com>
2021-10-01 12:51:09 +0200

    change condition of display of guests chip

--- client/components/Playlist/PlaylistEditionChipList.tsx
+++ client/components/Playlist/PlaylistEditionChipList.tsx
@@ -16,32 +16,26 @@ type PlaylistEditionChipListProps = {
   setInitialGuests: Setter<Guest[]>;
 };
 
-const PlaylistEditionChipList = (props: PlaylistEditionChipListProps) => {
-  if (props.playlist.guests !== undefined) {
-    return (
-      <>
-        {props.collection.map((elem, index) => (
-          <PlaylistEditionChipElement
-            elem={elem}
-            key={elem.id}
-            index={index}
-            playlist={props.playlist}
-            collection={props.collection}
-            setter={props.setter}
-            newChips={props.newChips}
-            playlistStatus={props.playlistStatus}
-            setPlaylistStatus={props.setPlaylistStatus}
-            guestPayload={props.guestPayload}
-            setGuestPayload={props.setGuestPayload}
-            initialGuests={props.initialGuests}
-            setInitialGuests={props.setInitialGuests}
-          />
-        ))}
-      </>
-    );
-  } else {
-    return null;
-  }
-};
+const PlaylistEditionChipList = (props: PlaylistEditionChipListProps) => (
+  <>
+    {props.collection.map((elem, index) => (
+      <PlaylistEditionChipElement
+        elem={elem}
+        key={elem.id}
+        index={index}
+        playlist={props.playlist}
+        collection={props.collection}
+        setter={props.setter}
+        newChips={props.newChips}
+        playlistStatus={props.playlistStatus}
+        setPlaylistStatus={props.setPlaylistStatus}
+        guestPayload={props.guestPayload}
+        setGuestPayload={props.setGuestPayload}
+        initialGuests={props.initialGuests}
+        setInitialGuests={props.setInitialGuests}
+      />
+    ))}
+  </>
+);
 
 export default PlaylistEditionChipList;
